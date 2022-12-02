ca62c5dea2ca79e1292d5a9679622406e411edc0
Kingston Armstrong <70528667+Kiingst@users.noreply.github.com>
2022-12-02 13:10:38 -0600

    ok

--- 3D/3Dplayer.gd
+++ 3D/3Dplayer.gd
@@ -1,6 +1,6 @@
 extends KinematicBody
 
-
+export (int) var speed = 50
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,5 +12,22 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var move_vec = Vector3()
+	var roll_speed2 = 1
+	if Input.is_action_pressed("Move_Up"):
+		move_vec.z -= 1
+	if Input.is_action_pressed("Move_Down"):
+		move_vec.z += 1
+	if Input.is_action_pressed("Move_Left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("Move_Right"):
+		move_vec.x += 1
+	if Input.is_action_pressed("Swing"):
+		pass
+	if Input.is_action_just_pressed("Roll"):
+		pass
+	move_vec = move_vec.normalized()
+	move_and_slide(move_vec * speed)
+	
+	
