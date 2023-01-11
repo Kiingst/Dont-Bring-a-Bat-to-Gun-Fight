494b61027b2702571e9af0f5a230546ea5990f8f
Kingston Armstrong <70528667+Kiingst@users.noreply.github.com>
2023-01-11 13:06:55 -0600

    Adding base player

---
+++ Assets/Player/Player_Script/Base_Player.gd
@@ -0,0 +1,69 @@
+extends KinematicBody2D
+
+export (int) var MOVE_SPEED = 300
+export (int) var health = 3
+var move_vec = Vector2.ZERO
+
+signal take_damage
+var alive = true
+var floor_normal = Vector2(0, -1)
+
+export var jump_height : float
+export var jump_time_to_peak : float
+export var jump_time_to_descent : float
+export var double_jump = true
+export var jump_count = 2
+var count_num = 0
+
+onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+var moving = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func Player_Control(delta):
+	move_vec.y += get_gravity() * delta
+	move_vec.x = get_input_velocity() * MOVE_SPEED
+	
+	if is_on_floor() == true:
+		count_num = 0
+	
+	if double_jump == false:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			jump()
+	else:
+		if Input.is_action_just_pressed("jump") and count_num != jump_count:
+			jump()
+			count_num += 1
+	
+	move_vec = move_and_slide(move_vec, Vector2.UP)
+	
+
+func _physics_process(delta):
+	if alive == false:
+		return
+	else:
+		Player_Control(delta)
+
+func get_gravity() -> float:
+	return jump_gravity if move_vec.y < 0.0 else fall_gravity
+func get_input_velocity() -> float:
+	var horizontal := 0.0
+	
+	if Input.is_action_pressed("Move_Left"):
+		horizontal -= 1.0
+	if Input.is_action_pressed("Move_Right"):
+		horizontal += 1.0
+	
+	return horizontal
+
+func jump():
+	move_vec.y = jump_velocity
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
